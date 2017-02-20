9a2b6f9fc13c394d620bd2dae02f4abba1a05c0a
George Eaton <george_eaton@hotmail.co.uk>
2017-02-20 12:41:13 +0000

    articleController can be instantiated

---
+++ spec/article-controller-spec.js
@@ -0,0 +1,11 @@
+describe("ArticleController", function() {
+  var articleController;
+
+  beforeEach(function() {
+    articleController = new ArticleController();
+  });
+
+  it("can be instantiated", function() {
+    expect(articleController).toBeTruthy();
+  });
+});
---
+++ src/article-controller.js
@@ -0,0 +1,12 @@
+(function(exports) {
+
+function ArticleController(){
+}
+
+ArticleController.prototype.replaceText = function() {
+  var news = document.getElementById("app");
+  greeting.innerHTML = "Article"
+}
+
+  exports.ArticleController = ArticleController;
+})(this);
