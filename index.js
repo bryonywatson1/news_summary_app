fb211f61c1a790bfcf5d66ae02243270e150094f
George Eaton <george_eaton@hotmail.co.uk>
2017-02-20 12:54:44 +0000

    adds index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+  var articleController = new ArticleController();
+};
--- src/article-controller.js
+++ src/article-controller.js
@@ -1,12 +1,17 @@
 (function(exports) {
 
-function ArticleController(){
-}
+  function ArticleController(){
+    this.articleList = new ArticleList();
+  }
 
-ArticleController.prototype.replaceText = function() {
-  var news = document.getElementById("app");
-  greeting.innerHTML = "Article"
-}
+  ArticleController.prototype.replaceText = function() {
+    var news = document.getElementById("app");
+    greeting.innerHTML = "Article"
+  }
+
+  ArticleController.prototype.createArticle = function(headline, content) {
+    this.articleList.createArticle(headline, content);
+  };
 
   exports.ArticleController = ArticleController;
 })(this);
